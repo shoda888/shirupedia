f794927ff93ccc3ee45d14b7467a8b06190bcac3
GEKKA888 <gekka888@gmail.com>
2018-06-11 12:18:12 +0900

    routes設定,Userモデル,Profileモデル作成,Spec環境作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  resource :sessions
+  resources :users
+  resources :profiles
+  resources :communities
+  resources :answers
+  resources :questions
 end
