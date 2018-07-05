a7bf1aa2c8f4c284f2edf17f6982795593b42221
GEKKA888 <gekka888@gmail.com>
2018-07-05 17:20:55 +0900

    タグ付け綺麗に

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :answers
 
   validates :email, format: { with: /.+@m.titech.ac.jp/ }
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates :password, presence: true, uniqueness: true, length: { in: 1..10 }
 end
