8c50f4ee4a675053422b85f954ed4795e5e5d91a
GEKKA888 <gekka888@gmail.com>
2018-07-10 01:14:55 +0900

    ひとまず、自由単語検索を実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,4 +15,12 @@ class Question < ApplicationRecord
       transitions :from => :wanted, :to => :completed
     end
   end
+
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Question.where(['title LIKE ?', "%#{search}%"])
+    else
+      Question.all #全て表示。
+    end
+  end
 end
