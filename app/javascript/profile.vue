342ebcc40916fe15cae9e2596159c4c3b86de6a8
GEKKA888 <gekka888@gmail.com>
2018-09-12 11:12:37 +0900

    プロフイールフォームにvueを適応する準備

---
+++ app/javascript/profile.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>
+        {{message}}
+    </p>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                message: "プロフィール入力!",
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 30px;
+        text-align: center;
+    }
+</style>
