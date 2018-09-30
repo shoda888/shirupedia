303bb67342e3890096881604a816d48ccd398c65
GEKKA888 <gekka888@gmail.com>
2018-10-01 00:08:05 +0900

    質問詳細ページに類似した質問を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require bootstrap/dist/js/bootstrap.min.js
 //= require admin-lte/dist/js/adminlte.min.js
 //= require bootstrap-tagsinput-latest/dist/bootstrap-tagsinput.js
-//= require footerFixed.js
 //= require dropzone
+//= stub footerFixed
 //= require_tree .
