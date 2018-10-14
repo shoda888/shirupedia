fe0a9e1295dd9a7a0ee36fecb453109b329c404b
GEKKA888 <gekka888@gmail.com>
2018-10-14 13:57:15 +0900

    プロフィール後で登録はなし

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def created_with_email(user)
+    @url = 'http://localhost:3000/profiles/new'
     mail(to: user.email, subject: 'welcome to shirupedia')
   end
 end
