07a6a4074740475c0975667a0811634e8a3e4e58
GEKKA888 <gekka888@gmail.com>
2018-11-02 14:29:21 +0900

    API::V1::AplicationControllerを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::ApplicationController
   def index
     @users = User.all
     @users = @users.map do |user|
