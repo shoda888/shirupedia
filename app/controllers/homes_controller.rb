86634bdb8e287f0c3b68c7d8891b7988feb28c8d
GEKKA888 <gekka888@gmail.com>
2018-12-25 21:32:31 +0900

    ogpを改良、画像の読み込みを改善

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,6 @@ class HomesController < ApplicationController
   layout 'home'
 
   def top
+    @ogp = Ogp.new({ title: "Shirupedia", description: "東工大の学生を対象に授業・講義、学生生活の疑問点を解消するアプリケーション", image: "https://res.cloudinary.com/hnj7qqu7w/image/upload/v1545738853/helloquence-s.jpg", card: "summary_large_image" })
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user1 = User.create(:name => "匿名シルペディア", :email => "shiru.p.aa@m.titech.ac.jp", :password => 'shirupedia98', :password_confirmation => 'shirupedia98')
-Profile.create(user_id: user1.id, avatar: File.open("./public/shirupedia_q_info.png"))
+Profile.create(user_id: user1.id, avatar: File.open("./public/shirupedia_q_info.png"), token: SecureRandom.hex(12))
 
 question1 = Question.create(:title => "微分方程式(1)の解法が分かりません", :user_id => user1.id, :field_list => ['微分方程式,院試,数学相談室,数学'], :aasm_state => 'wanted')
 Cover.create(:photo_message => File.open("./public/sample_q1.jpg"), coverable_id: question1.id, coverable_type: 'Question')
