32b1e8a7e592506b376f52328a109c6f563fc47b
GEKKA888 <gekka888@gmail.com>
2018-12-25 21:47:56 +0900

    テスト通し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
   protect_from_forgery with: :exception
-  # QuestionColor = %w(lime light-green green teal cyan light-blue ).freeze
   before_action :set_current_user
-  # before_action :request_ogp
-
-  # def request_ogp
-  #   @ogp = Ogp.new({ title: "Shirupedia", description: "東工大の学生を対象に授業・講義、学生生活の疑問点を解消するアプリケーション", image: "https://res.cloudinary.com/hnj7qqu7w/image/upload/v1544865660/fllxwb65ayzecqrfup2p.png" })
-  # end
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
