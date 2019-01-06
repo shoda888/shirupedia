a49a370b43fa4af9d021863a3828201e9b01e381
GEKKA888 <gekka888@gmail.com>
2019-01-06 12:57:25 +0900

    mailerの編集

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def created_with_email(user, token)
     @url = "http://localhost:3000/profiles/new?token=#{token}" if Rails.env.development?
     @url = "http://titech-shirupedia-stg.herokuapp.com/profiles/new?token=#{token}" if Rails.env.staging?
-    @url = "http://titech-shirupedia.herokuapp.com/profiles/new?token=#{token}" if Rails.env.production?
+    @url = "https://www.shirupedia.com/profiles/new?token=#{token}" if Rails.env.production?
     mail(to: user.email, subject: 'welcome to shirupedia')
   end
 end
