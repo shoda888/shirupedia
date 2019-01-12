91ee40e81b7a585b61f1a76bdcbc5bb85be57628
GEKKA888 <gekka888@gmail.com>
2019-01-12 19:01:24 +0900

    プロフィールにてSPA諦める

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $('select').formSelect();
+  // $('input.autocomplete').autocomplete({
+  //   data: {
+  //     "Apple": 'null',
+  //     "Microsoft": 'null',
+  //     "Google": 'null'
+  //   },
+  // });
 });
