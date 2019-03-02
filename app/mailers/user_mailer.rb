92527f6b9ab3725189277df25d7614705581add4
GEKKA888 <gekka888@gmail.com>
2019-03-02 16:10:54 +0900

    メールリンクを設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,15 @@
 class UserMailer < ApplicationMailer
   def created_with_email(user, token)
-    @url = "http://localhost:3000/profiles/new?token=#{token}" if Rails.env.development?
-    @url = "http://titech-shirupedia-stg.herokuapp.com/profiles/new?token=#{token}" if Rails.env.staging?
-    @url = "https://www.shirupedia.com/profiles/new?token=#{token}" if Rails.env.production?
+    if Rails.env.development?
+      @url = "http://localhost:3000/profiles/new?token=#{token}"
+      @deep = "shirupedia://profiles/new?token=#{token}"
+    elsif Rails.env.staging?
+      @url = "http://titech-shirupedia-stg.herokuapp.com/profiles/new?token=#{token}"
+      @deep = "shirupedia://profiles/new?token=#{token}"
+    else
+      @url = "https://www.shirupedia.com/profiles/new?token=#{token}"
+      @deep = "shirupedia://profiles/new?token=#{token}"
+    end
     mail(to: user.email, subject: 'welcome to shirupedia')
   end
 end
