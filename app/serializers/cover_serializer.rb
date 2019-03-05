457e90d2f66c03890496f2794ab1e6336da07375
GEKKA888 <gekka888@gmail.com>
2019-03-05 16:39:12 +0900

    日本語uri対応

--- app/serializers/cover_serializer.rb
+++ app/serializers/cover_serializer.rb
@@ -12,6 +12,8 @@
 #
 
 class CoverSerializer < ActiveModel::Serializer
+  require 'uri'
+  
   attributes :id, :url
   has_many :comments, serializer: CommentSerializer
 
@@ -19,9 +21,9 @@ class CoverSerializer < ActiveModel::Serializer
     if object.photo_message.present?
       object.photo_message.url
     elsif object.coverable.text_message.present?
-      "https://res.cloudinary.com/hajcm9hwg/image/upload/e_brightness:30,o_80/w_430,c_fit,l_text:Sawarabi%20Gothic_35:#{object.coverable.text_message}/dphwltrgho6nk3ys2nun.png"
+      "https://res.cloudinary.com/hajcm9hwg/image/upload/e_brightness:30,o_80/w_430,c_fit,l_text:Sawarabi%20Gothic_35:#{URI.escape(object.coverable.text_message)}/dphwltrgho6nk3ys2nun.png"
     else
-      "https://res.cloudinary.com/hajcm9hwg/image/upload/e_brightness:30,o_80/w_430,c_fit,l_text:Sawarabi%20Gothic_35:#{object.coverable.title}/dphwltrgho6nk3ys2nun.png"
+      "https://res.cloudinary.com/hajcm9hwg/image/upload/e_brightness:30,o_80/w_430,c_fit,l_text:Sawarabi%20Gothic_35:#{URI.escape(object.coverable.title)}/dphwltrgho6nk3ys2nun.png"
     end
   end
 end
