d1c17dcdc5f7944997dc91a2e43d4a0b5c64e1e0
GEKKA888 <gekka888@gmail.com>
2019-03-17 17:32:36 +0900

    コンパイル&テスト

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,12 +1,12 @@
 class BlocksController < ApplicationController
-    before_action :authenticate_user
-    def create
-        @user = User.find(params[:id])
-        @current_user.block(@user) unless @current_user.blocked?(@user)
-    end
+  before_action :authenticate_user
+  def create
+    @user = User.find(params[:id])
+    @current_user.block(@user) unless @current_user.blocked?(@user)
+  end
 
-    def destroy
-        @user = User.find(params[:id])
-        @current_user.unblock(@user) if @current_user.blocked?(@user)
-    end
+  def destroy
+    @user = User.find(params[:id])
+    @current_user.unblock(@user) if @current_user.blocked?(@user)
+  end
 end
